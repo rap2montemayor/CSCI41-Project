c2f3f5f4f95002e14288a9cb92a4a927975a0131
Sean Owen Chan <owen.chan@obf.ateneo.edu>
2021-12-10 20:20:57 +0800

    Updated Categories in Product.sql

--- Product.sql
+++ Product.sql
@@ -4,7 +4,7 @@ CREATE DATABASE Products;
 
 CREATE TABLE product(
 	product_number int PRIMARY KEY NOT NULL,
-	category varchar(12),
+	category varchar(20),
 	color VARCHAR(20) NOT NULL DEFAULT 'Black',
 	features varchar(255) NOT NULL DEFAULT ' ',
 	product_name varchar(50) NOT NULL DEFAULT '<EMPTY>',
@@ -12,6 +12,6 @@ CREATE TABLE product(
 	stock INT,
 	product_type --idk how to this one
 	CHECK
-	 (category IN (/*insert categories here*/))
+	 (category IN ('Office', 'Women\'s Health', 'Sports', 'Child Care', 'Digital Devices'))
 	
-)
+);
