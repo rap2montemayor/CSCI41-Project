d244d0f8599aec3e1b7efe60e9ee893b1d7b059b
Sean Owen Chan <owen.chan@obf.ateneo.edu>
2021-12-11 21:19:49 +0800

    added database

--- Initialize.sql
+++ Initialize.sql
@@ -9,3 +9,6 @@ DROP TABLE product_color_stock;
 DROP TABLE product_dimensional;
 DROP TABLE product_features;
 DROP TABLE product_slotted;
+DROP DATABASE pixiedustmain;
+
+CREATE DATABASE pixiedustmain;
