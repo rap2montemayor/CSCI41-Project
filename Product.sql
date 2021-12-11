7aa41a9bf85d1626b86319b74e9071a48961e35d
Raprap Montemayor <rap2montemayor@gmail.com>
2021-12-11 21:12:27 +0800

    Remade product subtypes, misc fixes

--- Product.sql
+++ Product.sql
@@ -2,10 +2,10 @@ CREATE TABLE product (
     PRIMARY KEY (product_id),
     product_id            INT            NOT NULL,
     category              VARCHAR(255)   NOT NULL,
+                          CHECK (category IN ('folders', 'pen organizers', 'planners')),
     name                  VARCHAR(255)   NOT NULL,
     personalization_limit INT            NOT NULL,
     price                 NUMERIC(19, 2) NOT NULL,
-    product_type          VARCHAR(255)   NOT NULL,
 );
 
 CREATE TABLE product_features (
@@ -14,18 +14,26 @@ CREATE TABLE product_features (
     feature    VARCHAR(255) NOT NULL,
 );
 
-CREATE TABLE product_slotted (
+CREATE TABLE product_pen_organizers (
     FOREIGN KEY (product_id) REFERENCES product(product_id),
     product_id INT NOT NULL,
     slots      INT NOT NULL,
 );
 
-CREATE TABLE product_dimensional (
+CREATE TABLE product_folders (
     FOREIGN KEY (product_id) REFERENCES product(product_id),
-    product_id INT NOT NULL,
-    height     INT NOT NULL,
-    length     INT NOT NULL,
-    width      INT NOT NULL,
+    product_id INT           NOT NULL,
+    height     NUMERIC(6, 2) NOT NULL,
+    length     NUMERIC(6, 2) NOT NULL,
+    width      NUMERIC(6, 2) NOT NULL,
+);
+
+CREATE TABLE product_planners (
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    product_id INT           NOT NULL,
+    height     NUMERIC(6, 2) NOT NULL,
+    length     NUMERIC(6, 2) NOT NULL,
+    width      NUMERIC(6, 2) NOT NULL,
 );
 
 CREATE TABLE product_color_stock (
