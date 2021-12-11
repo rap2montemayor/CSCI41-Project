efc11796cb030a34712f7fa3cb7c20234613379e
John Julius Da Silva <john.dasilva@obf.ateneo.edu>
2021-12-11 20:41:01 +0800

    Updated product_type based on suggestion.

--- Product.sql
+++ Product.sql
@@ -4,13 +4,14 @@ CREATE DATABASE Products;
 
 CREATE TABLE product(
 	product_number int PRIMARY KEY NOT NULL,
-	category varchar(20),
+	category varchar(20) NOT NULL CHECK
+	 (category IN ('Office', 'Health', 'Sports', 'Child Care', 'Digital Devices')),
 	color VARCHAR(20) NOT NULL DEFAULT 'Black',
 	features varchar(255) NOT NULL DEFAULT ' ',
 	product_name varchar(50) NOT NULL DEFAULT '<EMPTY>',
 	product_price FLOAT(2),
 	stock INT DEFAULT 0,
-	product_type --idk how to this one
-	CHECK
-	 (category IN ('Office', 'Health', 'Sports', 'Child Care', 'Digital Devices'))
+	product_type varchar(255) NOT NULL CHECK (product_type IN ('Slotted', 'Dimensional'))
+
+
 );
