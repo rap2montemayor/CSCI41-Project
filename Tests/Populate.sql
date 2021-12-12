5544781ee11fa64f84192f1423ed1161b0babbc4
Raprap Montemayor <rap2montemayor@gmail.com>
2021-12-12 13:15:30 +0800

    Added populate.sql

---
+++ Tests/Populate.sql
@@ -0,0 +1,51 @@
+INSERT INTO person (person_id, name)
+VALUES (1001, 'Diluc Ragnvindr'),
+       (1002, 'Venti');
+
+INSERT INTO agent (agent_id, person_id, customer_count)
+VALUES (2001, 1001, 0);
+
+INSERT INTO customer (customer_id, agent_id, person_id)
+VALUES (3002, 2001, 1002);
+
+INSERT INTO product (product_id, category, name, personalization_limit, price)
+VALUES (4001, 'planners', 'yung ninakaw na notebook ni albedo', 30, 12345),
+       (4002, 'folders', 'mga harana ni venti', 32, 31415),
+       (4003, 'pen organizers', 'pencil case ni xinqiu', 32, 12345);
+
+INSERT INTO product_features (product_id, feature)
+VALUES (4001, 'with instructions on how to make another albedo'),
+       (4001, 'includes sketches of sucrose'),
+       (4002, 'venti sadboi vibes'),
+       (4002, 'may kasamang paboritong kanta ni barbara'),
+       (4002, 'pampatulog ni xiao'),
+       (4003, 'may kasamang pencil sharpener');
+
+INSERT INTO product_pen_organizers (product_id, slots)
+VALUES (4003, 5);
+
+INSERT INTO product_folders (product_id, height, length, width)
+VALUES (4002, 10, 4, 20);
+
+INSERT INTO product_planners (product_id, height, length, width)
+VALUES (4001, 9, 8, 7);
+
+INSERT INTO product_color_stock (product_id, color, stock)
+VALUES (4001, 'red', 3),
+       (4001, 'orange', 1),
+       (4001, 'yellow', 4),
+       (4002, 'green', 1),
+       (4002, 'blue', 5),
+       (4002, 'purple', 9),
+       (4003, 'pink', 2),
+       (4003, 'black', 6),
+       (4003, 'red', 8);
+
+INSERT INTO orders (order_id, agent_id, customer_id, amount_due, order_date, delivery_address, gift)
+VALUES (5001, 2001, 3002, 20000, '2021-12-12', 'Windrise Tree', false);
+
+INSERT INTO ordered_product (order_id, product_id, discount, personalization, quantity)
+VALUES (5001, 4002, 0, 'XxBarbatosxX', 1);
+
+INSERT INTO order_recipient (order_id, person_id)
+VALUES (5001, 1002);
