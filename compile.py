87027e1bb6726051488193021d0ce0a2d21f0678
Raprap Montemayor <rap2montemayor@gmail.com>
2021-12-13 03:00:43 +0800

    Renamed compiled file

--- compile.py
+++ compile.py
@@ -7,7 +7,7 @@ files = [
 	'Order.sql',
 ]
 
-with open('a.sql', mode='w', encoding='utf-8') as output:
+with open('PixieDust.sql', mode='w', encoding='utf-8') as output:
 	for i in files:
 		with open(i, mode='r', encoding='utf-8') as sqlfile:
 			print(*sqlfile, file=output, sep='')
